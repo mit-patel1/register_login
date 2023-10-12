78d6dab4f9053244b30ad06760d13dc1ef07aa01
dev6intellial <dev6.intellial@gmail.com>
2023-10-12 11:34:31 +0530

    first commit

---
+++ myApp/accounts/urls.py
@@ -0,0 +1,11 @@
+from accounts import views
+from django.urls import path
+
+# app_name = "accounts"
+
+urlpatterns = [
+    path("", views.homepage, name="homepage"),
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name="logout"),
+]
